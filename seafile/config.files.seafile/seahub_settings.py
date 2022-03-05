1c765457ae6f37ef5c57e00ad24c10fb9ce7a376
strahli30 <100966094+strahli30@users.noreply.github.com>
2022-03-05 22:33:30 +0100

    Add files via upload

---
+++ seafile/config.files.seafile/seahub_settings.py
@@ -0,0 +1,5 @@
+#NUR URL von http auf https ändern
+FILE_SERVER_ROOT = "https://<Domain bspw. seafile.dyn.de>/seafhttp"
+
+#diese Zeile noch einfügen
+SERVICE_URL = 'https://<Domain bspw. seafile.dyn.de>/seafhttp'
